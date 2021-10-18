a2868a07b8ea7d167ee5289be247f8dc1b67c890
junha <terajunha@snu.ac.kr>
2021-10-18 17:16:38 +0900

    10.18

--- gridflowmodel.py
+++ gridflowmodel.py
@@ -6,3 +6,14 @@
 ## 16개 grid별 모델학습 이후 앙상블학습 (bagging) 을 통해 모델 학습
 ## 시계열 데이터를 t -> t+1 데이터의 집합으로 바꿔야함
 
+import pandas as pd
+import dask.dataframe as dd
+import math
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import gridflowpack.nearseoulgrid as nsgrid
+
+boundary, ingrid, allData = nsgrid.gridData()
+
--- gridflowpack/nearseoulgrid.py
+++ gridflowpack/nearseoulgrid.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 
@@ -17,8 +18,33 @@ def to_dic(array) :
         dic = np.append(dic,{'latitude':i[0],'longitude':i[1]})
     return dic
 
-def to_xarray(dic) :
+def to_xarray(dic, xr) :
+    ns_aq_data = pd.DataFrame()
     for loc in dic :
-        ns_aq_data = aerosol.sel(loc)
+        data = xr.sel(loc).expand_dims(["latitude","longitude"]).to_dataframe()
+        ns_aq_data = pd.concat([ns_aq_data,data]).sort_index()
 
-print(ns_aq_data)
+    return ns_aq_data
+
+def gridData() :
+    ae_bd = to_xarray(to_dic(boundary),aerosol)
+    # print("ad_bd",ae_bd)
+    ae_ig = to_xarray(to_dic(inner_grid),aerosol)
+    # print("ad_ig", ae_ig)
+    wt_bd = reset_Loc(to_xarray(to_dic(boundary),weather))
+    wt_ig = reset_Loc(to_xarray(to_dic(inner_grid), weather))
+
+    df_bd = pd.merge(ae_bd, wt_bd, left_index=True, right_index=True, how='left').sort_index()
+    df_ig = pd.merge(ae_ig, wt_ig, left_index=True, right_index=True, how='left').sort_index()
+    df_all = pd.concat([df_bd,df_ig]).sort_index()
+    # print(df_all)
+    return df_bd, df_ig, df_all
+
+def reset_Loc(df) :
+    df = df.reset_index(['time','latitude','longitude'])
+    df.latitude = np.round(df.latitude,1)
+    df.longitude = np.round(df.longitude, 1)
+    df = df.set_index(['time','latitude','longitude'])
+    return df
+
+gridData()
